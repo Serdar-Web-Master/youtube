c40760e581e619a5e1b0029a4be50cb7539e06c5
Serdar <serdarcholukov@yandex.com>
2022-07-25 19:20:37 +0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+import { a, b} from "./vars.js";
+
+console.log(a,b);
+
+const ssylka = document.querySelector('.ssylka');
+ssylka.addEventListener("click",f1);
+
+function f1 () {
+   console.log("ssylka");
+}
